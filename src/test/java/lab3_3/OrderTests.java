bf942867568dcf3491d283047e26badd96de439a
Kamil Błażejczuk <202119@edu.p.lodz.pl>
2018-05-05 10:48:19 +0200

    Utworzenie klasy testowej OrderTests

---
+++ src/test/java/lab3_3/OrderTests.java
@@ -0,0 +1,14 @@
+package lab3_3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
