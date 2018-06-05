881b3060de3c482341d003ded25e8ad66c2a6cd5
Artur Jankowski <artij1@o2.pl>
2018-06-05 18:47:35 +0200

    Completed the test, changing implementation of Order wasn't even required

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -1,8 +1,17 @@
 import edu.iis.mto.time.Order;
 import edu.iis.mto.time.OrderExpiredException;
 import edu.iis.mto.time.OrderItem;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.joda.time.DateTimeUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 
 public class OrderTest {
     private Order order;
@@ -12,11 +21,14 @@ public class OrderTest {
     {
         order = new Order();
         item = new OrderItem();
+        order.addItem(item);
+        order.submit();
+        DateTimeUtils.setCurrentMillisFixed(new DateTime().getMillis()+108000000);
     }
 
     @Test (expected = OrderExpiredException.class)
     public void expiredOrderConfirmationShouldThrowException()
     {
-        
+        order.confirm();
     }
 }
